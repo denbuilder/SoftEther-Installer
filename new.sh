4268a04a626a74fa8677ac99418f47b4be6cdc2b
Denish Nathan <denishbuilder10@gmail.com>
2018-04-06 15:01:06 +0800

    ADD

--- new.sh
+++ new.sh
@@ -31,6 +31,44 @@ link="http://www.softether-download.com/files/softether/"$version"-tree/Linux/So
 wget $link
 tar xzf "$file"
 
-#Enter file
+#Enter file and setting up
 cd vpnserver
 make i_read_and_agree_the_license_agreement
+cd ..
+mv vpnserver /usr/local
+cd /usr/local/vpnserver/
+chmod 600 *
+chmod 700 vpnserver
+chmod 700 vpncmd
+mkdir /var/lock/subsys
+touch /etc/init.d/vpnserver
+cat > /etc/init.d/vpnserver <<EOF
+#!/bin/sh
+# chkconfig: 2345 99 01
+# description: SoftEther VPN Server
+DAEMON=/usr/local/vpnserver/vpnserver
+LOCK=/var/lock/subsys/vpnserver
+test -x $DAEMON || exit 0
+case "$1" in
+start)
+$DAEMON start
+touch $LOCK
+;;
+stop)
+$DAEMON stop
+rm $LOCK
+;;
+restart)
+$DAEMON stop
+sleep 3
+$DAEMON start
+;;
+*)
+echo "Usage: $0 {start|stop|restart}"
+exit 1
+esac
+exit 0
+EOF
+mkdir /var/lock/subsys
+chmod 755 /etc/init.d/vpnserver && /etc/init.d/vpnserver start
+update-rc.d vpnserver defaults
